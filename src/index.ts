bd5be1ee0f22ba40c24a200f1654aa033ed35ed4
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-01-15 18:59:46 -0300

    Typescript Tipos Basicos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+// Tipos Básicos
 let age: number = 5;
-const nome: string = "Mauricio";
-const isValid: Boolean = true
+const firtsName: string = "Mauricio";
+const isValid: Boolean = true;
+let idk: any = 6;
+
+idk = "12";
+
+const ids: number[] = [1,2,3,4,5,6,7];
+const family: string[] = ["Mauricio", "Matheus"]
 
 console.log(age);
