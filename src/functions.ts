f258ad67a0099058ae7aee489f78e068f68c0f26
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-01-15 19:27:18 -0300

    Typescript Função

---
+++ src/functions.ts
@@ -0,0 +1,10 @@
+// Função
+const sum = (x: number, y: number): number | string => {
+     return x + y;
+};
+
+const value = sum(1, 2);
+
+const log = (message: string): void => {
+    console.log(message)
+};
