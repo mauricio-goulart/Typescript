8495d50342b48b7eef4f370af9ccd08615d594fd
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-01-15 22:29:47 -0300

    Typescript Union e Interfaces

--- src/objects.ts
+++ src/objects.ts
@@ -12,11 +12,39 @@ type User = {
     work: Works[]
 };
 
- const user: User = {
+const user: User = {
     firstName: "Mauricio",
     age: 20,
     password: "1234",
     work: [{function: "Dev", location: "Brazil"}, {function: "Dev2", location: "Brazil"}]
  };
 
- console.log(user.email!)
+console.log(user.email!);
+
+ // Unions
+type Author = {
+    books: string[];
+ };
+
+const author: Author & User = {
+    firstName: "Mauricio",
+    age: 19,
+    work: [{function: "dev", location: "Brazil"}],
+    books: ["The Witcher"]
+ };
+
+ // Interfaces
+
+interface UserInterface {
+    readonly firstName: string;
+    email: string;
+ };
+
+const emailUser: UserInterface = {
+    email: "mauricio@gmail.com",
+    firstName: "Mm"
+ }
+
+emailUser.email = "2"
+//emailUser.firstName = "mm" - erro readonly
+
